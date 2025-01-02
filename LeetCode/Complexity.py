aebf8051b189987a66f3b6cbb8a5aca4be0723d6
Derian Rojas <116598387+derianbv@users.noreply.github.com>
2025-01-02 18:09:27 -0500

    Update Complexity.py

--- LeetCode/Complexity.py
+++ LeetCode/Complexity.py
@@ -8,7 +8,9 @@ EPP:  Es una función que denota qué tanto cambia la cantidad de operaciones se
 Ejemplo: para una complejidad O(n), siginifica que si le meto un n de datos le va a tomar n operaciones,
 si duplico n entonces se duplicará el tiempo que le tome 
 
-
+n generalmente representa el número de elementos en una lista, cadena, árbol, grafo, o cualquier dato que esté procesando.
+O(n) se encarga de medir el peor de los casos, por ejemplo, si quiero buscar un elemento en una lista no ordenada de n elementos 
+es posible que en el peor de los casos tenga que hacer n operaciones para encontrar el número 
 
 
 ''''
