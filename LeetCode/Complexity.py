8f65dd99840cfd03895e7b36234e3d30af312fd1
Derian Rojas <116598387+derianbv@users.noreply.github.com>
2025-01-02 18:23:49 -0500

    Update Complexity.py

--- LeetCode/Complexity.py
+++ LeetCode/Complexity.py
@@ -12,13 +12,16 @@ n generalmente representa el número de elementos en una lista, cadena, árbol, 
 O(n) se encarga de medir el peor de los casos, por ejemplo, si quiero buscar un elemento en una lista no ordenada de n elementos 
 es posible que en el peor de los casos tenga que hacer n operaciones para encontrar el número 
 
-Tabla de complejidad: 
+---------------------- Tabla de complejidad: ---------------------------------------------------------------- 
 Por ejemplo, tengo un arreglo de n= 10 elementos, así se demorará el algoritmo en funcionar: 
-
-1. 0(n!) => 10 datos y O(10!): 3628800 operaciones.
-2. 0(2^n) => 10 datos y O(2^10): 1024 operaciones.
-3. O(n) => 10 datos y O(10): 10 operaciones. 
-
+s
+1. 0(n!) =   10 datos => O(10!): 3628800 operaciones.
+2. 0(2^n) =   10 datos => O(2^10): 1024 operaciones.
+3. 0(n^2) =   10 datos => O(10^2): 100 operaciones.
+4. O(n*Log_2(n)) =   10 datos => O(10*Log_2(10)): 33.21 operaciones. 
+5. O(n) =   10 datos => O(10): 10 operaciones. 
+6. O(log_2(n)) =   10 datos => O(Log_2(10)):  3.21 operaciones. 
+7. O 
 
 
 
