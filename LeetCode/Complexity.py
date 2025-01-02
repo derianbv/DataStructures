9a6b1df30ddf8e489507e174e3b76c2761a705d9
Derian Rojas <116598387+derianbv@users.noreply.github.com>
2025-01-02 18:02:24 -0500

    Update Complexity.py

--- LeetCode/Complexity.py
+++ LeetCode/Complexity.py
@@ -1,1 +1,14 @@
-#################################  Big O notation ##############################################
+#################################  Big O notation ###############################################
+#Page to calc complexity: 
+#https://www.bigocalc.com/
+
+
+  '''
+EPP:  Es una función que denota qué tanto cambia la cantidad de operaciones segun una entrada n de items. 
+Ejemplo: para una complejidad O(n), siginifica que si le meto un n de datos le va a tomar n operaciones,
+si duplico n entonces se duplicará el tiempo que le tome 
+
+
+
+
+''''
