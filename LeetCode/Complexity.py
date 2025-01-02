78efed2bfa27eaf3b69fb39fbac5fb48e888e704
Derian Rojas <116598387+derianbv@users.noreply.github.com>
2025-01-02 18:35:24 -0500

    Update Complexity.py

--- LeetCode/Complexity.py
+++ LeetCode/Complexity.py
@@ -24,5 +24,18 @@ Por ejemplo, tengo un arreglo de n= 10 elementos, así se demorará el algoritmo
 7. O(1) =   10 datos => O(1): 1 operación.  
 
 
+-------------------------------- Simplificación ----------------------------------------------------------
+La complejidad Big O se basa en un estudio asintotico, es decir para entradas muy grandes o muy dominantes, se pueden simplificar ya que solo importa un acercamiento general del credimiento de pasos segun la cantidad de entradas:
 
+1. O(n) + O(1) = O(n) 
+
+2. O(3n) = O(n): no importa la pendiente de la funcion, por cada uno salen 3, ya que solo importa ver si es lineal o no.  
+
+3. O(5n + 1 + n) = O(6n + 1) = O(6n) = O(n): DOMINANTE. 
+
+4. O(n+m) = si n > m = O(n) y viceversa: DOMINANTE, acá el tiempo de ejecución depende de dos entradas independientes: n y m. s
+
+5. O(n^2 + n^3) = O(n^3): DOMINANTE
+
+6. 
 ''''
